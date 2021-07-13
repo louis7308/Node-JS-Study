0a290a212556588f4ebc98c09a6ed3b037e68938
전승원 <78971821+louis7308@users.noreply.github.com>
2021-07-13 11:28:49 +0900

    폴더 구조 최적화

--- 2Dir/app/app.js
+++ 2Dir/app/app.js
@@ -4,10 +4,10 @@ const app = express();
 
 
 // 라우팅 분리
-const home = require('./routers/index')
+const home = require('./src/routers/index')
 
 
-app.set('views', '../views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs')
 
 app.use('/', home); // use -> 미들웨어를 등록해주는 메소드
